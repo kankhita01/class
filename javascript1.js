c7b53c18f044ca8a4109f962ee939acd73affcc1
Jarin Tasnim Kankhita <159518487+kankhita01@users.noreply.github.com>
2024-06-08 21:40:55 +0600

    Add files via upload

---
+++ javascript1.js
@@ -0,0 +1,12 @@
+/*console.log("Welcome to java script");
+console.log("javascript journey begins...");
+alert("Day 1 with javascript");*/
+
+
+var box1 = 10;
+var box2 = 20;
+var result = box1+box2;
+//console.log(result);
+
+
+console.log("sum of two number ",result);
